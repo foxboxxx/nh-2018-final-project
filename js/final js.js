d7442b0b91e84f5eb5b6a2dda10ed05b2281b31e
T4L3NT <36267395+T4L3NT@users.noreply.github.com>
2018-04-27 21:24:42 -0400

    Fixes

--- js/final js.js
+++ js/final js.js
@@ -4,24 +4,10 @@ function goneImage(){
 $("#hide").click( () => {
 	goneImage();
 	});
-/*function seeImage(){
-	$(".rollingdice").show();
-	}
-$(".show").click( () => {
-	seeImage();
-	});
-function reApear() {
-	var x = document.getElementByClass("rollingdice");
-	if (x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none"
-	}
-}*/
+
 	function timedImage() {
 	setTimeout(function myFunction() {
 	var myimages=new Array()
-	//specify random images below. You can have as many as you wish
 	myimages[1]="http://www.clker.com/cliparts/m/v/m/J/4/V/dice-1-md.png"
 	myimages[2]="http://www.clker.com/cliparts/a/Y/E/o/z/t/dice-2-md.png"
 	myimages[3]="http://www.clker.com/cliparts/M/e/P/O/L/b/dice-3-md.png"
@@ -62,16 +48,7 @@ function doShow(){
 function doTheHide() {
 	$("#rollingdice").delay(2000).fadeOut();
 }
-/*function timedImage() {
-	setTimeout(function myFunction() {
-    var x = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("demo").innerHTML = x;
-}, 2500);}
-function timedImage2() {
-	setTimeout(function myFunction() {
-    var x = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("demo2").innerHTML = x;
-}, 2500);}*/
+
 
 
 
